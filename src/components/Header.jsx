480e8d14bda2b90b7c5f01a139c5123726cf7cc1
Dipti Rani <gdipti642@gmail.com>
2021-10-23 10:07:45 +0530

    added Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Container, Box, AppBar, Toolbar, List, ListItemButton, ListItemText } from '@mui/material';
+
+function Header () {
+    return (
+        <>
+            <Container fixed>
+                        <AppBar position="fixed" color="transparent" elevation={0}>
+                            <Toolbar sx={{ display: 'flex', justifyContent: 'center' }}>
+                                <List component="nav" aria-label="secondary mailbox folder" style={{ display: 'flex', flexDirection: 'row' }}>
+                                    <ListItemButton >
+                                        <ListItemText primary="Home" />
+                                    </ListItemButton>
+                                    <ListItemButton >
+                                        <ListItemText primary="About" />
+                                    </ListItemButton>
+                                    <ListItemButton >
+                                        <ListItemText primary="Education" />
+                                    </ListItemButton>
+                                    <ListItemButton >
+                                        <ListItemText primary="Certification" />
+                                    </ListItemButton>
+                                    <ListItemButton >
+                                        <ListItemText primary="Project" />
+                                    </ListItemButton>
+                                    <ListItemButton >
+                                        <ListItemText primary="Skills" />
+                                    </ListItemButton>
+                                    <ListItemButton >
+                                        <ListItemText primary="Contact" />
+                                    </ListItemButton>
+                                </List>
+                            </Toolbar>
+                        </AppBar>
+                </Container>
+        </>
+    )
+}
+
+export default Header;
